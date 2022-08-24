c8fd61b0c94eb67924a11651928d3c7de6b123b9
Satoshi-Sh <satoss1108@gmail.com>
2022-08-24 16:06:04 -0500

    Complete UI part

--- game.js
+++ game.js
@@ -1,26 +1,94 @@
 
 const hands = ['Rock','Paper','Scissors'];
 const computer = getComputerChoice();
-let score = 0
+const result = document.querySelector('.result');
+let p_score = 0;
+let c_score = 0;
 
-// play five games
-for (let i = 0; i <5; i++) {
-  const playerHand = prompt("Choose your hand Rock, Paper, Scissors");
-  const computerHand = getComputerChoice()
+const pHand = document.querySelector('.p-hand');
+const cHand = document.querySelector('.c-hand');
 
-  const sentence = playRound(playerHand,computerHand);
-  console.log(sentence);
+const pScore = document.querySelector('.p-score');
+const cScore = document.querySelector('.c-score');
+
+// add trigger to play again
+
+const playButton = document.querySelector('#again');
+playButton.onclick= refresh;
+
+// add trigger to buttons;
+const buttons = document.querySelectorAll('.buttons button');
+buttons.forEach((button)=>{
+ button.addEventListener('click',(e)=>
+ {
+   const playerHand = e.target.innerText;
+   const computerHand = getComputerChoice()
+   
+   pHand.innerText=playerHand;
+   cHand.innerText= computerHand;
+   
+   const sentence = playRound(playerHand,computerHand);
+
+   
+
+   result.innerText= sentence;
+   // keep score
+   if (sentence[4]=="W"){
+    p_score++;
+    result.style.color='green';
+    } 
+    else if(sentence[4]==['L']){
+    c_score++;
+    result.style.color='red';
+}
+    else{
+        result.style.color='black';
+    }
+   // reach 5:
+   if (p_score==5){
+    pHand.innerText= "Player";
+    cHand.innerText= "Wins";
+    cHand.style.color='Green';
+    playAgain();
+
+   }
+   else if(c_score==5){
+    pHand.innerText= "Computer"
+    cHand.innerText= "Wins"
+    cHand.style.color='Red';
+    playAgain();
+   }
+   
+   //show updated score 
+   pScore.innerText= p_score;
+   cScore.innerText= c_score;
+ })
+});
+
   
-  // keep score
-  if (sentence[4]=="W"){
-    score++;
-  } 
-  
+function playAgain(){
+    //hide hands button
+    document.querySelector('.buttons').style.display='none';
+    // show play again button;
 
-
+    document.querySelector('.restart').style.display='flex'; 
 }
 
-console.log(`You won ${score} game(s)`)
+function refresh(){
+   //show hands button
+   document.querySelector('.buttons').style.display='flex';
+   // hide play again button;
+
+   document.querySelector('.restart').style.display='none';
+   pHand.innerText='';
+   cHand.innerText='';
+   p_score=0;
+   c_score=0;
+
+   pScore.innerText=0;
+   cScore.innerText=0;
+   result.innerText='';
+}
 
 
 function getComputerChoice() {
